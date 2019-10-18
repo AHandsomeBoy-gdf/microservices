83854d463513c22c70ab05aa2ef2a35c16e8f729
hahec <875847186@qq.com>
2019-10-18 17:50:53 +0800

    add zuul

---
+++ api-geteway/src/main/java/com/example/apigeteway/ApiGetewayApplication.java
@@ -0,0 +1,35 @@
+package com.example.apigeteway;
+
+import com.example.apigeteway.Filter.AccessFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ApiGetewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGetewayApplication.class, args);
+    }
+
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
+    }
+
+    /**
+     * 自定义路由映射规则
+     * @return
+     */
+    @Bean
+    public PatternServiceRouteMapper serviceRouteMapper() {
+        return new PatternServiceRouteMapper(
+          "(?<name>^.+)-(?<version>v.+$)",
+                "$(version)/$(name)"
+        );
+    }
+
+}
