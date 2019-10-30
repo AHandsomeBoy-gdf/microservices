e35493c152b428b596127d17a154df3848846e7c
hahec <875847186@qq.com>
2019-10-30 17:02:53 +0800

    add zuul

--- api-geteway/src/main/java/com/example/apigeteway/ApiGetewayApplication.java
+++ api-geteway/src/main/java/com/example/apigeteway/ApiGetewayApplication.java
@@ -1,9 +1,16 @@
 package com.example.apigeteway;
 
 import com.example.apigeteway.Filter.AccessFilter;
+import com.example.apigeteway.Filter.DidiFilterProcessor;
 import com.example.apigeteway.Filter.ThrowExceptionFilter;
+import com.example.apigeteway.error.DidiErrorAttributes;
+import com.netflix.zuul.FilterProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.SearchStrategy;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +20,7 @@ import org.springframework.context.annotation.Bean;
 public class ApiGetewayApplication {
 
     public static void main(String[] args) {
+        FilterProcessor.setProcessor(new DidiFilterProcessor());
         SpringApplication.run(ApiGetewayApplication.class, args);
     }
 
@@ -25,6 +33,13 @@ public class ApiGetewayApplication {
 //    public ThrowExceptionFilter throwExceptionFilter() {
 //        return new ThrowExceptionFilter();
 //    }
+
+    @Bean
+    @ConditionalOnMissingBean(value = ErrorAttributes.class, search = SearchStrategy.CURRENT)
+    public DefaultErrorAttributes errorAttributes() {
+        //启动自定义的异常信息
+        return new DidiErrorAttributes();
+    }
 
     /**
      * 自定义路由映射规则
